182bb1cfa5a8e8cde81c5b47e443a74c6290339d
Nathan Ladd <nathanmladd@gmail.com>
2021-07-04 21:34:32 -0400

    Trying to get sprites loaded in.

--- src/game/player.asm
+++ src/game/player.asm
@@ -1,150 +1,390 @@
+;-------------
+; Player routines
+;-------------
 INCLUDE "hardware.inc"
+SECTION "Player",ROM0
 
-SECTION "player", ROMX
+PlayerSpawnX    equ 126
+PlayerSpawnY    equ 180
+PlayerFlags     equ %00000000
+PlayerTileBegin equ 44
+PlayerTileEnd   equ 88
+PlayerTilePad   equ 9
+PlayerTileWidth equ 3
+PlayerOAMEnd    equ 36
+PlayerWidth     equ 24
+PlayerHeight    equ 24
+PlayerVelCap    equ 6
+PlayerHP        equ 5
 
-PLAYER_START_X EQU (160 / 2 - 20)
-PLAYER_START_Y EQU (144 / 2 + 16)
-PLAYER_MAX_VEL EQU $04
+PlayerFSpeed    equ 8
+PlayerFShoot    equ 1
+PlayerFRunStart equ 2
+PlayerFRunEnd   equ 6
+PlayerFJump     equ 6
+PlayerFIdle     equ 7
+PlayerFShootAir equ 8
+PlayerFShootRun equ 9
 
-player_sprite_init::
-    ; Set variables
-    ld [HL], $00
-    ld HL, player_x
-    ld [HL], PLAYER_START_X
-    ld HL, player_y
-    ld [HL], PLAYER_START_Y
-    ; Enable sprite
-    ld HL, rLCDC
-    set 1, [HL]
-    ; Set Palette
-    ; ld HL, OBJ0_PAL
-    ; ld [HL], %11100100
-    ; ld HL, OBJ1_PAL
-    ; ld [HL], %11010000
-    ; Set Attributes
-    ; Top left
-    ld HL, $FE00
-    ld [HL], PLAYER_START_Y
-    inc L
-    ld [HL], PLAYER_START_X
-    inc L
-    ld [HL], $80
-    inc L
-    set 4, [HL]
-    ; Top right
-    ld HL, $FE04
-    ld [HL], PLAYER_START_Y
-    inc L
-    ld [HL], PLAYER_START_X + 8
-    inc L
-    ld [HL], $81
-    inc L
-    set 4, [HL]
-    ; Bottom right
-    ld HL, $FE08
-    ld [HL], PLAYER_START_Y + 8
-    inc L
-    ld [HL], PLAYER_START_X + 8
-    inc L
-    ld [HL], $82
-    inc L
-    set 4, [HL]
-    ; Bottom left
-    ld HL, $FE0C
-    ld [HL], PLAYER_START_Y + 8
-    inc L
-    ld [HL], PLAYER_START_X
-    inc L
-    ld [HL], $83
-    inc L
-    set 4, [HL]
-    ret
+PLAYER_LOAD::
+  ;init a load of crap
+  ld  a,PlayerSpawnY
+  ld  [player_y],a
+  ld  a,PlayerSpawnX
+  ld  [player_x],a
+  ld  a,PlayerFJump
+  ld  [player_frame],a
+  ld  a,PlayerFRunStart
+  ld  [player_fstart],a
+  ld  a,PlayerFRunEnd
+  ld  [player_fend],a
+  xor a
+  ld  [player_idle],a
+  ld  [player_flags],a
+  ld  [player_fcount],a
+  ld  [player_gcount],a
+  ld  [rSCX],a
+  ld  [rSCY],a
+  ld  a,1
+  ld  [player_yvel],a
+  ld  a,PlayerHP
+  ld  [player_hp],a
+  ret
 
-; player_update::
-;     ; update animation
-;     ld HL, player_frame_index
-;     ld A, [HL]
-;     ld HL, player_frames
-;     ld L, A
-;     ld A, [HL] ; Got frame
-;     ld HL, $FE0E
-;     ld [HL], A
-;     ld HL, player_frame_index
-;     inc [HL]
-;     ld A, [HL]
-;     cp 27
-;     jr nz, .player_update_continue_00
-;     ld [HL], $00
-; .player_update_continue_00:
-;     ld HL, player_dead
-;     ld A, [HL]
-;     cp $00
-;     jr nz, .player_update_no_input
-;     ; Check if Button A 
-;     ; was pressed
-;     ld HL, IO_P15
-;     bit BUTTON_A, [HL]
-;     jr nz, .player_update_no_input
-;     ld HL, IO_P15_OLD
-;     bit BUTTON_A, [HL]
-;     jr z, .player_update_no_input
-    
-;     ; Set the player to active
-;     ld HL, player_active
-;     ld [HL], $01
+; PLAYER_DIE:
+;   ld  a,1
+;   ld  [game_over],a
+;   ret
 
-;     ; if button A was pressed
-;     ; we apply jump
-; .player_update_jump:
-;     ld HL, player_vel_y
-;     ld A, [HL]
-;     ld A, -(PLAYER_MAX_VEL * 2)
-;     ld [HL], A
-; .player_update_no_input:
-;     ld HL, player_active
-;     ld A, [HL]
-;     cp $00
-;     jr nz, .player_update_apply_accel_y
 
-; player_update_end:
-;     ret
+PLAYER_CHECK_TOP:
+  ;de = player_y/8
+  xor a
+  ld  d,a
+  ld  b,a
+  ld  a,[player_y]
+  sub 8
+  ld  e,a
+  ld  c,8
+  call DIVIDE
+  
+  ;y tile index
+  push de
 
-player_set_position::
-    xor A
-    ld HL, player_y
-    ld B, [HL]
-    ld HL, player_x
-    ld C, [HL]
-    ; Top left
-    ld HL, $FE00
-    ld [HL], B
-    inc L
-    ld [HL], C
-    ; Top right
-    ld HL, $FE04
-    ld E, C
-    ld A, C
-    adc A, 8
-    ld C, A
-    ld [HL], B
-    inc L
-    ld [HL], C
-    ; Bottom left
-    ld HL, $FE0C
-    ld A, B
-    adc A, 8
-    ld B, A
-    ld [HL], B
-    inc L
-    ld [HL], E
-    ; Bottom right
-    ld HL, $FE08
-    ld [HL], B
-    inc L
-    ld [HL], C
-    
-    ret
+  ;de = player_x/8
+  xor a
+  ld  d,a
+  ld  b,a
+  ld  a,[player_x]
+  ;sub 8
+  ld  e,a
+  ld  c,8
+  call DIVIDE
 
-SECTION "player_vars", WRAM0
-player_x:: DS 1
-player_y:: DS 1
+  ;d = x, e = y tile index
+  ld  d,e
+  pop bc
+  ld  e,c
+
+  ;get tile value at index
+  ld  c,0
+  call GET_TILE
+  ret
+
+PLAYER_CHECK_SIDE:
+  ;de = player_y/8
+  xor a
+  ld  d,a
+  ld  b,a
+  ld  a,[player_y]
+  sub 2
+  ld  e,a
+  ld  c,8
+  push hl
+  call DIVIDE
+  pop hl
+  
+  ;y tile index
+  push de
+
+  ;de = player_x/8
+  xor a
+  ld  d,a
+  ld  b,a
+
+  ;if l 0 then right else left
+  ld  a,l
+  cp  0
+  jr  z,.right
+
+  ld  a,[player_x]
+  sub 2
+  jr  .con
+
+.right
+  ld  a,[player_x]
+  add 10
+
+.con
+  ld  e,a
+  ld  c,8
+  call DIVIDE
+
+  ;d = x, e = y tile index
+  ld  d,e
+  pop bc
+  ld  e,c
+
+  ;get tile value at index
+  ld  c,1
+  call GET_TILE
+  ret  
+
+PLAYER_MOVE:
+;   call PLAYER_SHOOT
+
+  ;check dpad left
+  ld  a,[joypad_down]
+  call JOY_LEFT
+  jr  z,.left
+
+  ;check dpad left
+  ld  a,[joypad_down]
+  call JOY_RIGHT
+  jr  z,.right
+
+  ;set idle frame
+  ld  a,1
+  ld  [player_idle],a
+  jr  .up
+
+.left
+  ;check left
+  ld  l,1
+  call PLAYER_CHECK_SIDE
+
+  ;check if tile is empty
+  cp  0
+  jr  nz,.set_nonidle
+  ld  a,b
+  cp  0
+  jr  nz,.set_nonidle
+
+  ;move left
+  ld  a,[player_x]
+  sub 2
+  ld  [player_x],a 
+  ld  a,[player_flags]
+  or  1
+  ld  [player_flags],a
+  jr  .set_nonidle
+
+.right
+  ;check right
+  ld  l,0
+  call PLAYER_CHECK_SIDE
+
+  ;check if tile is empty
+  cp  0
+  jr  nz,.set_nonidle
+  ld  a,b
+  cp  0
+  jr  nz,.set_nonidle
+
+  ;move left
+  ld  a,[player_x]
+  add 2
+  ld  [player_x],a
+  ld  a,[player_flags]
+  and 2
+  ld  [player_flags],a
+
+.set_nonidle
+  xor a
+  ld  [player_idle],a
+
+.up
+  ;check if grounded
+  ld  a,[player_ground]
+  cp  0
+  jr  z,.end
+
+  ;check dpad up
+  ld  a,[joypad_pressed]
+  call JOY_A
+  jr  nz,.end
+
+.end
+  ret
+
+PLAYER_UPDATE::
+  ;check game won
+;   ld  a,[game_won]
+;   cp  0
+;   jr  z,.update
+
+;   ld  a,[joypad_pressed]
+;   call JOY_START
+;   jr  nz,.update
+
+;   jp START
+
+.update
+  call PLAYER_MOVE
+
+.end
+  ret
+
+PLAYER_OAM:
+  ;load the first tile addr
+  ld  a,PlayerTileBegin
+  ld  [player_tile],a
+
+  ;frame number
+  ld  a,[player_frame]
+  ld  b,a
+
+.loop
+  ;for each frame value
+  ld  a,b
+  dec a
+  ld  b,a
+  jr  nz,.next_frame
+  jr  .check_flip
+
+.next_frame
+  ;add padding for each tile index
+  ld  a,[player_tile]
+  add a,PlayerTilePad
+  ld  [player_tile],a
+  jr  .loop
+
+.check_flip
+  ;check flags
+  ld  a,[player_flags]
+  and 1
+  jr  z,.set_tiles
+
+  ;offset tile value
+  ld  a,[player_tile]
+  add a,2
+  ld  [player_tile],a
+
+.set_tiles
+  ;begin setting oam data
+  ld  a,[player_tile]
+  ld  b,a
+  add PlayerTilePad
+  ld  e,a
+
+  ld  hl,my_sprites
+
+  ;c is y counter
+  ;d is x counter
+  ld  c,0
+  ld  d,0
+  xor a
+  ld [arb_counter],a
+
+  ;set temp vars
+  ld  a,[player_y]
+  ld  [player_y_temp],a
+  ld  a,[player_x]
+  ld  [player_x_temp],a
+
+.loop_set
+  ;check if we are on a new row
+  ld  a,c
+  cp  PlayerTileWidth
+  jr  nz,.set_y
+
+  ;offset y
+  ld  a,[player_y_temp]
+  add a,8
+  ld  [player_y_temp],a
+  
+  ;reset c
+  xor a
+  ld  c,a
+
+.set_y
+  ;y position
+  ld  a,[player_y_temp]
+  ld  [hli],a
+
+  ;inc y counter
+  ld  a,c
+  inc a
+  ld  c,a
+
+  ld  a,d
+  cp  PlayerTileWidth
+  jr  nz,.set_x
+
+  ld  d,0
+  ld  a,[player_x]
+  ld  [player_x_temp],a
+
+.set_x
+  ;x position
+  ld  a,[player_x_temp]
+  ld  [hli],a
+
+  ;offset x
+  ld  a,d
+  inc a
+  ld  d,a
+  ld  a,[player_x_temp]
+  add a,8
+  ld  [player_x_temp],a
+
+  ;check flags
+  ld  a,[player_flags]
+  and 1
+  jr  z,.dont_flip
+
+.flip
+  ;tile
+  ld  a,b
+  ld  [hli],a
+  dec a
+  ld  b,a
+
+  ;check if we need to reset
+  ld  a,[arb_counter]
+  inc a
+  ld  [arb_counter],a
+  cp  3
+  jr  nz,.flip_flags
+
+  ;reset?
+  xor a
+  ld  [arb_counter],a
+  ld  a,b
+  add 6
+  ld  b,a
+
+.flip_flags
+  ;flags
+  ld  a,%00100000
+  ld  [hli],a
+  jr  .end
+
+.dont_flip
+  ;tile
+  ld  a,b
+  ld  [hli],a
+  inc a
+  ld  b,a
+
+  ;flags
+  xor a
+  ld  [hli],a
+
+.end
+  ;are we done yet?
+  ld  a,b
+  cp  e
+  jr  nz,.loop_set
+
+  ret
---
+++ src/game/vars.asm
@@ -0,0 +1,30 @@
+SECTION "RAM Vars",WRAM0[$C100]
+
+	joypad_down::    DB
+	joypad_pressed:: DB
+	
+	;player vars
+	bullets_alive::DB
+	player_ground::DB
+	player_idle::  DB
+	player_fset::  DB
+	player_fcount::DB
+	player_gcount::DB
+	player_fstart::DB
+	player_fend::  DB
+	player_y::     DB
+	player_x::     DB
+	player_y_temp::DB
+	player_x_temp::DB
+	player_yvel::  DB
+	player_frame:: DB
+	player_tile::  DB
+	player_flags:: DB
+	player_hp::    DB
+
+	;other
+	arb_counter::  DB
+
+	my_sprites:: DS 4*13
+
+	GAME_MAP_DATA:: DB ;bad
