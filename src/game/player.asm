f1ca02efad40d90f200197aa6c783dc523cc8880
Nathan Ladd <nathanmladd@gmail.com>
2021-07-11 20:31:39 -0400

    Cleaning up.

--- src/game/player.asm
+++ src/game/player.asm
@@ -4,6 +4,7 @@ INCLUDE "hardware.inc"
 
 PLAYER_START_X EQU (160 / 2 - 20)
 PLAYER_START_Y EQU (144 / 2 + 16)
+PLAYER_BALLOON_START_Y EQU (PLAYER_START_Y - 16)
 
 player_sprite_init::
   ; Set variables
@@ -12,8 +13,46 @@ player_sprite_init::
   ld HL, player_y
   ld [HL], PLAYER_START_Y
   ; Set Attributes
+  ; BALLOON *****
   ; Top left
   ld HL, wShadowOAM
+  ld [HL], PLAYER_BALLOON_START_Y
+  inc L
+  ld [HL], PLAYER_START_X
+  inc L
+  ld [HL], $84
+  inc L
+  ld [HL], %00000000
+  ; Top right
+  ld HL, wShadowOAM+4
+  ld [HL], PLAYER_BALLOON_START_Y
+  inc L
+  ld [HL], PLAYER_START_X + 8
+  inc L
+  ld [HL], $84
+  inc L
+  ld [HL], %00100000
+  ; Bottom right
+  ld HL, wShadowOAM+8
+  ld [HL], PLAYER_BALLOON_START_Y + 8
+  inc L
+  ld [HL], PLAYER_START_X + 8
+  inc L
+  ld [HL], $83
+  inc L
+  ld [HL], %00100000
+  ; Bottom left
+  ld HL, wShadowOAM+12
+  ld [HL], PLAYER_BALLOON_START_Y + 8
+  inc L
+  ld [HL], PLAYER_START_X
+  inc L
+  ld [HL], $83
+  inc L
+  ld [HL], %00000000
+  ; CACTUS *****
+  ; Top left
+  ld HL, wShadowOAM+16
   ld [HL], PLAYER_START_Y
   inc L
   ld [HL], PLAYER_START_X
@@ -22,7 +61,7 @@ player_sprite_init::
   inc L
   ld [HL], %00000000
   ; Top right
-  ld HL, wShadowOAM+4
+  ld HL, wShadowOAM+20
   ld [HL], PLAYER_START_Y
   inc L
   ld [HL], PLAYER_START_X + 8
@@ -31,7 +70,7 @@ player_sprite_init::
   inc L
   ld [HL], %00100000
   ; Bottom right
-  ld HL, wShadowOAM+8
+  ld HL, wShadowOAM+24
   ld [HL], PLAYER_START_Y + 8
   inc L
   ld [HL], PLAYER_START_X + 8
@@ -40,7 +79,7 @@ player_sprite_init::
   inc L
   ld [HL], %00100000
   ; Bottom left
-  ld HL, wShadowOAM+12
+  ld HL, wShadowOAM+28
   ld [HL], PLAYER_START_Y + 8
   inc L
   ld [HL], PLAYER_START_X
