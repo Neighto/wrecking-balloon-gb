dfd9e6284f6721eaba35fd9af542ffb205e36af2
Nathan Ladd <nathanmladd@gmail.com>
2021-07-14 22:50:20 -0400

    More sweet movement.

--- src/core/joypad.asm
+++ src/core/joypad.asm
@@ -1,49 +1,97 @@
-SECTION "joypad", ROMX
+SECTION "joypad", ROM0
 
 INCLUDE "hardware.inc"
 
-read_joypad::
-    ; Read P14
-    ld HL, rP1
-    ld A, $20
-    ld [HL], A
-    ld A, [HL]
-    ld HL, IO_P14
-    ld B, [HL]
-    ld [HL], A
-    ld HL, IO_P14_OLD
-    ld [HL], B
+ReadInput::
+	;select dpad
+	ld  a,%00100000
+  
+	;takes a few cycles to get accurate reading
+	ld  [_IO],a
+	ld  a,[_IO]
+	ld  a,[_IO]
+	ld  a,[_IO]
+	ld  a,[_IO]
+	
+	;complement a
+	cpl
+  
+	;select dpad buttons
+	and %00001111
+	swap a
+	ld  b,a
+  
+	;select other buttons
+	ld  a,%00010000
+  
+	;a few cycles later..
+	ld  [_IO],a  
+	ld  a,[_IO]
+	ld  a,[_IO]
+	ld  a,[_IO]
+	ld  a,[_IO]
+	cpl
+	and %00001111
+	or  b
 
-    ; Read P15
-    ld HL, rP1
-    ld A, $10
-    ld [HL], A
-    ld A, [HL]
-    ld HL, IO_P15
-    ld B, [HL]
-    ld [HL], A
-    ld HL, IO_P15_OLD
-    ld [HL], B
-    
-    ; Reset
-    ld HL, rP1
-    ld A, $FF
-    ld [HL], A
-    ret
+	; 1 indicates pressed 0 indicates unpressed
+	ld  b,a
+	ld  a,[joypad_down]
+	cpl
+	and b
+	ld  [joypad_pressed],a
+	ld  a,b
+	ld  [joypad_down],a
+	ret
 
-clear_joypad::
-    ld HL, IO_P14
-    ld [HL], $EF
-    ld HL, IO_P15
-    ld [HL], $DF
-    ld HL, IO_P14_OLD
-    ld [HL], $EF
-    ld HL, IO_P15_OLD
-    ld [HL], $DF
-    ret
-
-SECTION "joypad_vars", WRAM0
-IO_P14:: DS 1
-IO_P15:: DS 1
-IO_P14_OLD:: DS 1
-IO_P15_OLD:: DS 1
+JOY_RIGHT::
+	and %00010000
+	; cp  %00010000
+	jp  z, JOY_FALSE
+	ld  a, $1
+	ret
+JOY_LEFT::
+	and %00100000
+	; cp  %00100000
+	jp  z, JOY_FALSE
+	ld  a, $1
+	ret
+JOY_UP::
+	and %01000000
+	cp  %01000000
+	jp  nz, JOY_FALSE
+	ld  a, $1
+	ret
+JOY_DOWN::
+	and %10000000
+	cp  %10000000
+	jp  nz, JOY_FALSE
+	ld  a, $1
+	ret
+JOY_A::
+	and %00000001
+	cp  %00000001
+	jp  nz, JOY_FALSE
+	ld  a, $1
+	ret
+JOY_B::
+	and %00000010
+	cp  %00000010
+	jp  nz, JOY_FALSE
+	ld  a, $1
+	ret
+JOY_SELECT::
+	and %00000100
+	cp  %00000100
+	jp  nz, JOY_FALSE
+	ld  a, $1
+	ret
+JOY_START::
+	and %00001000
+	cp  %00001000
+	jp  nz, JOY_FALSE
+	ld  a, $1
+	ret
+JOY_FALSE:
+	ld  a, $0
+	ret
--- src/main.asm
+++ src/main.asm
@@ -39,14 +39,10 @@ GAMELOOP:
 	call WAIT_VBLANK
 
 	; Scroll screen
-	; call VBlank_HScroll
-
-	call ReadInput
-	ld  a,[joypad_down]
-	call JOY_RIGHT
-	jr  nz, .skip
 	call VBlank_HScroll
-.skip
+
+	; Player updates
+	call PlayerMovement
 	
   	; Call DMA subroutine to copy the bytes to OAM for sprites begin to draw
 	ld  a, HIGH(wShadowOAM)
@@ -125,108 +121,3 @@ LoadGameData:
 	; Initialize player
 	call InitializePlayer
 	ret
-
-SECTION "Input", rom0
-
-ReadInput:
-	;select dpad
-	ld  a,%00100000
-  
-	;takes a few cycles to get accurate reading
-	ld  [_IO],a
-	ld  a,[_IO]
-	ld  a,[_IO]
-	ld  a,[_IO]
-	ld  a,[_IO]
-	
-	;complement a
-	cpl
-  
-	;select dpad buttons
-	and %00001111
-	swap a
-	ld  b,a
-  
-	;select other buttons
-	ld  a,%00010000
-  
-	;a few cycles later..
-	ld  [_IO],a  
-	ld  a,[_IO]
-	ld  a,[_IO]
-	ld  a,[_IO]
-	ld  a,[_IO]
-	cpl
-	and %00001111
-	or  b
-	
-	;you get the idea
-	ld  b,a
-	ld  a,[joypad_down]
-	cpl
-	and b
-	ld  [joypad_pressed],a
-	ld  a,b
-	ld  [joypad_down],a
-	ret
-  
-  JOY_RIGHT:
-	and %00010000
-	cp  %00010000
-	jp  nz,JOY_FALSE
-	ld  a,$1
-	ret
-  JOY_LEFT:
-	and %00100000
-	cp  %00100000
-	jp  nz,JOY_FALSE
-	ld  a,$1
-	ret
-  JOY_UP:
-	and %01000000
-	cp  %01000000
-	jp  nz,JOY_FALSE
-	ld  a,$1
-	ret
-  JOY_DOWN:
-	and %10000000
-	cp  %10000000
-	jp  nz,JOY_FALSE
-	ld  a,$1
-	ret
-  JOY_A:
-	and %00000001
-	cp  %00000001
-	jp  nz,JOY_FALSE
-	ld  a,$1
-	ret
-  JOY_B:
-	and %00000010
-	cp  %00000010
-	jp  nz,JOY_FALSE
-	ld  a,$1
-	ret
-  JOY_SELECT:
-	and %00000100
-	cp  %00000100
-	jp  nz,JOY_FALSE
-	ld  a,$1
-	ret
-  JOY_START:
-	and %00001000
-	cp  %00001000
-	jp  nz,JOY_FALSE
-	ld  a,$1
-	ret
-  JOY_FALSE:
-	ld  a,$0
-	ret
-
-; 	set 4, [hl]
-; 	ld a, [hl]
-; 	or 0
-; 	; bit 1 is 1 on a => not pressed, if 0 => pressed
-; 	jr nz, .ReadInputEnd
-; 	call VBlank_HScroll
-; .ReadInputEnd
-; 	ret
