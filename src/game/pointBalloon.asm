cee0a2a9f858b265597f084c2f1339323c5995b1
Nathan Ladd <nathanmladd@gmail.com>
2021-07-30 19:01:04 -0400

    Made balloon pop animation nicer.

--- src/game/pointBalloon.asm
+++ src/game/pointBalloon.asm
@@ -96,6 +96,80 @@ FloatPointBalloonUp:
     ld [hl], a
     ret
 
+PopBalloonAnimation:
+    ; Check what frame we are on
+    ld a, [point_balloon_popping_frame]
+    cp a, 0
+    jr z, .frame0
+
+    ld a, [balloon_pop_timer]
+	inc	a
+	ld [balloon_pop_timer], a
+    cp a, 30
+    jp nz, .end
+
+    ; Can do next frame
+    ; Reset timer
+    xor a ; ld a, 0
+    ld [balloon_pop_timer], a
+    ; Check what frame we are on
+    ld a, [point_balloon_popping_frame]
+    cp a, 1
+    jp z, .frame1
+    cp a, 2
+    jp z, .clear
+    ret
+
+.frame0:
+    ; Popped left - frame 0
+    ld hl, point_balloon+2
+    ld [hl], $88
+    inc l
+    ld [hl], %00000000
+    ; Popped right - frame 0
+    ld hl, point_balloon+6
+    ld [hl], $88
+    inc l
+    ld [hl], %00100000
+    ld hl, point_balloon_popping_frame
+    ld [hl], 1
+    ret
+.frame1:
+    ; Popped left - frame 1
+    ld hl, point_balloon+2
+    ld [hl], $8A
+    inc l
+    ld [hl], %00000000
+    ; Popped right - frame 1
+    ld hl, point_balloon+6
+    ld [hl], $8A
+    inc l
+    ld [hl], %00100000
+    ld hl, point_balloon_popping_frame
+    ld [hl], 2
+    ret
+.clear:
+    ; Remove sprites
+    xor a ; ld a, 0
+    ld hl, point_balloon
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hl], a
+    ; Reset variables
+    ld hl, point_balloon_popping
+    ld [hl], a
+    ld hl, balloon_pop_timer
+    ld [hl], a
+    ld hl, point_balloon_popping_frame
+    ld [hl], a
+.end:
+    ret
+
 PointBalloonUpdate::
     ; Check if alive
     ld a, [point_balloon_alive]
@@ -133,10 +207,10 @@ DeathOfPointBalloon::
     ld hl, point_balloon_popping
     ld [hl], 1
     ; Remove from sprites
-    ld hl, point_balloon
-    ld [hli], a
-    ld [hli], a
-    ld [hli], a
-    ld [hl], a
+    ; ld hl, point_balloon
+    ; ld [hli], a
+    ; ld [hli], a
+    ; ld [hli], a
+    ; ld [hl], a
     ; TODO: for some reason I don't have to clear from point_balloon+4 ??
     ret
