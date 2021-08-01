8d1c400c140f86d038c6918f43a6d6da27c7a26c
Nathan Ladd <nathanmladd@gmail.com>
2021-08-01 16:57:45 -0400

    Bad math algorithms for getting 3 digits of score shown.

--- src/game/math.asm
+++ src/game/math.asm
@@ -2,18 +2,45 @@ INCLUDE "hardware.inc"
 
 SECTION "math", ROMX
 
+MODULO::
+    ; simple modulo formula
+    ; takes 'a' argument as number being modded
+    ; takes 'b' argument as mod number
+    ; returns 'a' as remainder
+    cp a, b
+    ret c
+.loop:
+    sub a, b
+    cp a, b
+    jr nc, .loop
+    ret
+
 RANDOM::
-    ; takes 'a' argument as mod number
+    ; takes 'a' argument as range number
+    ; returns 'a' as random number => 0 to a - 1
     ; ex:
     ; ld a, 10
     ; call RANDOM
     ; [a is # 0-9]
     ld b, a
-.seed:
     ldh a, [rDIV]
+    call MODULO
+    ret
+
+DIVISION::
+    ; simple division formula
+    ; takes 'a' argument as number being divided
+    ; takes 'b' argument as divider
+    ; uses 'c' as counter
+    ; returns 'a' as solution
+    ld c, 0
+    cp a, b
+    jr c, .end
 .loop:
     sub a, b
-    ; If a < b continue
+    inc c
     cp a, b
     jr nc, .loop
+.end:
+    ld a, c
     ret
