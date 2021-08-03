3fceb94ab24b9db9da044fdc13f78fc6246ea0b2
Nathan Ladd <nathanmladd@gmail.com>
2021-08-02 23:25:23 -0400

    Opting to use BCD now!

--- src/game/score.asm
+++ src/game/score.asm
@@ -6,70 +6,129 @@ InitializeScore::
 	ld [hl], a
     inc l
     ld [hl], a
+    inc l
+    ld [hl], a
+    ret
+
+ToBCD::
+    ; a = argument non-BCD number
+    ; a = return BCD number
+    ; double dabble
+    ld b, 0
+    ld c, 0 ;not use right now
+
+    ld d, 7 ;maybe 8
+.loop:
+    ; if BCD digit (and 00001111) is >= 5 (0101), then increment by 3 (0011) at that BCD digit
+    ; left shift one bit
+
+    ld e, a ; hold me
+
+    ld a, b ; grab the BCDs
+    and %00001111
+    cp a, 4
+    jr nc, .next
+    ; b >= 5
+    add %00000011 ;inc b by 3
+    ; TODO: now I have to re-add it to b
+    
+.next:
+
+    ; if b lower is >= 5
+    ;   increment it by 3
+    ;
+    
+
+    sla a ;if carry add it to b after shift!
+    jr c, .noCarry
+    sla b
+    inc b
+    jr .after
+.noCarry:
+    sla b
+.after:
+    dec d
+    jr nz, .loop
+    ; cp a, 9
+    ; jr c, .end 
+    ; add 6
+    ; TODO: NEED TO MAKE IT BCD-FRIENDLY IF BIG!!!
+.end:
     ret
 
 AddPoints::
-    ; b = points to receive (must be 8-bit)
+    ; b = points to receive (must be 1 byte BCD [max 99])
     ld a, [player_alive]
     and 1
     jr z, .end
     ; Alive we can add points
     ld hl, score ; 1st byte of score
     ld a, b
+    call ToBCD
     ; Add to hl, if there's a carry, then add to hl+1
     add [hl]
+    daa
     ld [hl], a 
     jr nc, .noCarry
     inc l ; 2nd byte of score
-    inc [hl]
+    ld a, 1 ; The carry value
+    add [hl]
+    daa
+    ld [hl], a 
+    jr nc, .noCarry
+    inc l ; 3rd byte of score
+    ld a, 1 ; The carry value
+    add [hl]
+    daa
+    ld [hl], a
 .noCarry:
     call RefreshScore
 .end:
     ret
 
-GetScore::
-    ; returns hl = score
-    ld a, [score]
-    ld l, a
-    ld a, [score+1]
-    ld h, a
-    ret
-
 GetScoreFromIndex::
     ; a = index
     ; a = return score
-
+    ld hl, score
     cp a, 0
     jr z, .firstDigit
     cp a, 1
     jr z, .secondDigit
+    inc l ; next byte
     cp a, 2
     jr z, .thirdDigit
-    ; ...
-
+    cp a, 3
+    jr z, .fourthDigit
+    inc l ; next byte
+    cp a, 4
+    jr z, .fifthDigit
+    cp a, 5
+    jr z, .sixthDigit
+    ret
 .firstDigit:
-    call GetScore
-	ld a, l ; get score 1st byte
-	ld b, 10
-	call MODULO
+    ld a, [hl]
+    and %00001111
     ret
 .secondDigit:
-    call GetScore
-	ld a, l ; get score 1st byte
-	ld b, 100
-	call MODULO
-	ld b, 2
-	call DIVISION ; should be able to do a bit shift...
-	ld b, 5
-	call DIVISION
+    ld a, [hl]
+    swap a
+    and %00001111
     ret
 .thirdDigit:
-	call GetScore
-	ld a, l ; get score 1st byte
-	ld b, 1000
-	call MODULO
-	ld b, 20
-	call DIVISION ; should be able to do a bit shift...
-	ld b, 5
-	call DIVISION
+    ld a, [hl]
+    and %00001111
+    ret
+.fourthDigit:
+    ld a, [hl]
+    swap a
+    and %00001111
+    ret
+.fifthDigit:
+    ld a, [hl]
+    and %00001111
+    ret
+.sixthDigit:
+    ld a, [hl]
+    swap a
+    and %00001111
     ret
