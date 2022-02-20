f9200a18210746b0624f3636d4f923d154811573
Nathan Ladd <nathanmladd@gmail.com>
2022-02-20 00:05:28 -0500

    Changing up city a bit needs work though.

--- src/main.asm
+++ src/main.asm
@@ -94,13 +94,13 @@ SetupNextLevel::
 	jr z, .level3
 	; Don't reach this point
 .level1:
-	; call SetLevel1Interrupts
-	; call LoadLevel1Graphics
-	; jr .endLevelSetup
+	call SetLevel1Interrupts
+	call LoadLevel1Graphics
+	jr .endLevelSetup
 .level2:
-	; call SetLevel2Interrupts
-	; call LoadLevel2Graphics
-	; jr .endLevelSetup
+	call SetLevel2Interrupts
+	call LoadLevel2Graphics
+	jr .endLevelSetup
 .level3:
 	call SetLevel3Interrupts
 	call LoadLevel3Graphics
