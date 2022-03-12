1448459b8c5e5d32ea530958e5d6b32fa1a1b452
Nathan Ladd <nathanmladd@gmail.com>
2022-03-12 18:21:18 -0500

    Better opening cutscene.

--- src/openingCutscene.asm
+++ src/openingCutscene.asm
@@ -2,7 +2,12 @@ INCLUDE "macro.inc"
 INCLUDE "hardware.inc"
 INCLUDE "constants.inc"
 
-HAND_WAVE_START_X EQU 152
+OPENING_CUTSCENE_UPDATE_TIME EQU %00000011
+OPENING_CUTSCENE_PAUSE_LENGTH EQU 15
+
+HAND_DOWN_START_X EQU 149
+HAND_DOWN_START_Y EQU 105
+HAND_WAVE_START_X EQU 151
 HAND_WAVE_START_Y EQU 96
 HAND_WAVE_TILE_1 EQU $3E
 HAND_WAVE_TILE_2 EQU $40
@@ -10,12 +15,17 @@ HAND_WAVE_TILE_2 EQU $40
 SECTION "opening cutscene vars", WRAM0
     wHandWavingFrame:: DB
     wHandWaveOAM:: DB
+    wOpeningCutsceneFrame:: DB
+    wOpeningCutsceneTimer:: DB
 
 SECTION "opening cutscene", ROMX
 
 InitializeOpeningCutscene::
 	xor a ; ld a, 0
 	ld [wHandWavingFrame], a
+    ld [wOpeningCutsceneFrame], a
+    ld a, OPENING_CUTSCENE_PAUSE_LENGTH
+    ld [wOpeningCutsceneTimer], a
     ret
 
 LoadOpeningCutsceneGraphics::
@@ -37,13 +47,13 @@ SpawnHandWave::
     ld a, b
 	ld [wHandWaveOAM], a
 	SET_HL_TO_ADDRESS wOAM, wHandWaveOAM
-    ld a, HAND_WAVE_START_Y
+    ld a, HAND_DOWN_START_Y
     ld [hli], a
-    ld a, HAND_WAVE_START_X
+    ld a, HAND_DOWN_START_X
     ld [hli], a
     ld [hl], HAND_WAVE_TILE_1
     inc l
-    ld [hl], OAMF_PAL0
+    ld [hl], OAMF_PAL0 | OAMF_YFLIP
 	ret
 
 HandWaveAnimation::
@@ -71,73 +81,93 @@ HandWaveAnimation::
 	ret
 
 UpdateOpeningCutscene::
-.fadeIn:
-    call FadeInPalettes
-    cp a, 0
-    ret z
-.hasFadedIn:
-    ld a, [wTriggerFadeOut]
-    cp a, 0
-    jr nz, .fadeOut
-    ld a, [wPlayerY]
-    add 4 ; Buffer for extra time before screen switch
-    ld b, a
-    ld a, SCRN_Y + OFF_SCREEN_ENEMY_BUFFER
-    cp a, b
-    jr nc, .skipFade
-    ld a, SCRN_VY - OFF_SCREEN_ENEMY_BUFFER
-    cp a, b
-    jr c, .skipFade
-.offscreen:
-    ld a, 1 
-    ld [wTriggerFadeOut], a
-    jr .skipFade
-.fadeOut:
-    call FadeOutPalettes
-    cp a, 0
-    jp nz, SetupNextLevel
-.skipFade:
-    call HandWaveAnimation
-.moveUp:
-    ; Bob up and down
+.checkWreckingBalloon:
+.canFlyUp:
     ldh a, [hGlobalTimer]
-    and %00111111
-    jr nz, .noBob
+    and %00000011
+    jr nz, .endCanFlyUp
+    ld a, [wOpeningCutsceneFrame]
+    cp a, 4
+    jr nc, .bobUp
+.endCanFlyUp:
+    ldh a, [hGlobalTimer]
+    and %00011111
+    ld d, %00000000
+    jr nz, .endWreckingBalloonCheck
     ld a, 1
     ld [wPlayerSpeed], a
     ld a, [wPlayerBobbedUp]
     cp a, 0
-    jr nz, .bobDown
-.bobUp:
-    ld a, 1
-    ld [wPlayerBobbedUp], a
-    ld d, %01000000
-    call MovePlayerForCutscene
-    ret
+    jr z, .bobUp
 .bobDown:
     xor a ; ld a, 0
     ld [wPlayerBobbedUp], a
     ld d, %10000000
+    jr .endWreckingBalloonCheck
+.bobUp:
+    ld a, 1
+    ld [wPlayerBobbedUp], a
+    ld d, %01000000
+.endWreckingBalloonCheck:
     call MovePlayerForCutscene
-    ret
-.noBob:
-    ld d, %00000000
-    call MovePlayerForCutscene
-
-;     ld a, [wPlayerY]
-;     add 16
-;     cp a, 75
-;     ldh a, [hGlobalTimer]
-;     jr c, .flyUpFast
-; .flyUpNormal:
-;     and %00000111
-;     ret nz
-;     jr .flyUp
-; .flyUpFast:
-;     and %00000001
-;     ret nz
-; .flyUp:
-;     ld a, 1
-;     ld [wPlayerSpeed], a
-;     call MovePlayerUpForCutscene
+    
+.updates:
+    ldh a, [hGlobalTimer]
+    and OPENING_CUTSCENE_UPDATE_TIME
+    cp a, 0
+    ret nz
+    ld a, [wOpeningCutsceneFrame]
+    cp a, 0
+    jr z, .fadeIn
+    cp a, 1
+    jr z, .pause
+    cp a, 2
+    jr z, .pause
+    cp a, 3
+    jr z, .pause
+    cp a, 4
+    jr z, .pause
+    cp a, 5
+    jr z, .triggerHandWave
+    push af
+    call HandWaveAnimation
+    pop af
+    cp a, 6
+    jr z, .pause
+    cp a, 7
+    jr z, .pause
+    cp a, 8
+    jr z, .fadeOut
+    jp SetupNextLevel
+.fadeIn:
+    call FadeInPalettes
+    cp a, 0
+    ret z
+    jr .endFrame
+.pause:
+    ld a, [wOpeningCutsceneTimer]
+    dec a 
+    ld [wOpeningCutsceneTimer], a
+    cp a, 0
+    ret nz
+    ld a, OPENING_CUTSCENE_PAUSE_LENGTH
+    ld [wOpeningCutsceneTimer], a
+    jr .endFrame
+.triggerHandWave:
+    SET_HL_TO_ADDRESS wOAM, wHandWaveOAM
+    ld a, HAND_WAVE_START_Y
+    ld [hli], a
+    ld a, HAND_WAVE_START_X
+    ld [hli], a
+    inc l
+    ld [hl], OAMF_PAL0
+    jr .endFrame
+.fadeOut:
+    call FadeOutPalettes
+    cp a, 0
+    ret z
+.endFrame:
+    ld a, [wOpeningCutsceneFrame]
+    inc a
+    ld [wOpeningCutsceneFrame], a
     ret
