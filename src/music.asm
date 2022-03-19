cfe2465a5dc90b7ca0843e17e70b1add467cbeba
Nathan Ladd <nathanmladd@gmail.com>
2022-03-19 16:35:31 -0400

    Fixed game over score not being total.

---
+++ src/music.asm
@@ -0,0 +1,29 @@
+include "src/include/hUGE.inc"
+
+; My own music creator for simple jingles that will be light
+SECTION "music", ROMX
+
+; gameOverTheme::
+;     dn C_4,1,$C0C
+;     dn ___,0,$000
+;     dn ___,0,$000
+;     dn ___,0,$E00
+;     dn B_3,1,$C0C
+;     dn ___,0,$000
+;     dn ___,0,$000
+;     dn ___,0,$E00
+;     dn A_3,1,$C0C
+;     dn ___,0,$000
+;     dn ___,0,$E00
+;     dn B_3,2,$E02
+;     dn B_3,1,$C0E
+;     dn A_3,1,$C0C
+;     dn ___,0,$E00
+;     dn F#3,1,$C0C
+;     dn ___,0,$000
+;     dn ___,0,$000
+;     dn ___,0,$000
+;     dn ___,0,$000
+;     dn ___,0,$E00
+;     dn LAST_NOTE,0,0
+
